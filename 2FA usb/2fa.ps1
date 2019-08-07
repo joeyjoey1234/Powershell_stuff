f96fad72c888ef14ea6821186527df830e107c46
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-08-06 23:54:57 -0600

    Add files via upload

---
+++ 2FA usb/2fa.ps1
@@ -0,0 +1,27 @@
+# Set-ExecutionPolicy RemoteSigned needs to be ran prior
+
+$password = Read-Host -Prompt 'Input your password' -AsSecureString
+Function Get-StringHash([String] $String,$HashName = "MD5")
+{
+$StringBuilder = New-Object System.Text.StringBuilder
+[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{
+[Void]$StringBuilder.Append($_.ToString("x2"))
+}
+$StringBuilder.ToString()
+}
+
+$password_hash = Get-StringHash $password "MD5"
+$hash_check = 'ad9b7cace3dc076b32296d0da661c4bd'
+
+if($password_hash -eq $hash_check)
+{
+    mkdir C:/.catsyndrome/chrome/storage/cache/browser/history/huh > $null
+    cp ./id_rsa.pub C:/.catsyndrome/chrome/storage/cache/browser/history/huh/ > $null
+    ./VeraCryptPortable/App/VeraCrypt/VeraCrypt.exe /s /q /v huh.txt /l X /a /password $password /pim 485 /k C:\.catsyndrome\chrome\storage\cache\browser\history\huh\id_rsa.pub /k C:\.catsyndrome\chrome\storage\cache\browser\history\huh
+    Read-Host -Prompt 'press enter when your done'
+    ./VeraCryptPortable/App/VeraCrypt/VeraCrypt.exe /s /q /d /wipecache /l X
+    rm -r C:\.catsyndrome\
+    exit
+}else {
+    exit
+}
