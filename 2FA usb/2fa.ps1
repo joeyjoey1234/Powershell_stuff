49e592f89784f7e6dddccd6b250053c273c58574
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-08-07 01:01:41 -0600

    Add files via upload

--- 2FA usb/2fa.ps1
+++ 2FA usb/2fa.ps1
@@ -1,22 +1,4 @@
-# Set-ExecutionPolicy RemoteSigned needs to be ran prior
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
 
-if ($myWindowsPrincipal.IsInRole($adminRole))
-   {
-   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-   clear-host
-   }
-else
-   {
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess);
-    exit
-   }
 mode con: cols=53 lines=10
 
 $password = Read-Host -Prompt 'Input your password' -AsSecureString
@@ -41,13 +23,8 @@ if($password_hash -eq $hash_check) {
     explorer.exe X:
     Read-Host -Prompt 'press enter when your done'
     ./VeraCryptPortable/App/VeraCrypt/VeraCrypt.exe /s /q /d /wipecache /l X
+    ./CCleaner/CCleaner.exe /AUTO
     rm -r C:\.catsyndrome\
-    del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
-    del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
-    del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
-    taskkill /f /im explorer.exe
-    start explorer.exe
-    wevtutil el | Foreach-Object {wevtutil cl "$_"}
     exit
 }else {
     exit
