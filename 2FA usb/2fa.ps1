b99eb3430dcd2917e5caee31e7cc503debb1924f
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-08-22 10:22:27 -0600

    Update
    Added Chrome Profile Mobility, Place chrome Profile in ChromeData Folder on root of X: AKA VeraCrypt mounted drive. Name folder Profile 1
    Added clean up and Copy of new profile on to drive
    
    Added echo of current status and Login loop if password is wrong

--- 2FA usb/2fa.ps1
+++ 2FA usb/2fa.ps1
@@ -15,17 +15,45 @@ $StringBuilder.ToString()
 $password_hash = Get-StringHash $password "MD5"
 $hash_check = 'ad9b7cace3dc076b32296d0da661c4bd'
 
-if($password_hash -eq $hash_check) {
+Function Login-in{ 
     mkdir C:/.catsyndrome/chrome/storage/cache/browser/history/huh > $null
     cp ./id_rsa.pub C:/.catsyndrome/chrome/storage/cache/browser/history/huh/ > $null
     ./VeraCryptPortable/App/VeraCrypt/VeraCrypt.exe /s /q /v huh.txt /l X /a /password $password /pim 485 /k C:\.catsyndrome\chrome\storage\cache\browser\history\huh\id_rsa.pub /k C:\.catsyndrome\chrome\storage\cache\browser\history\huh
-    Start-Sleep -s 8
+    echo "Sleeping for 15 sec Worsecase mount time"
+    Start-Sleep -s 15
+    Stop-Process -Name chrome -Force
+    echo "Removing any profile there is currently for chrome"
+    Remove-Item -Path "$($env:USERPROFILE)\AppData\Local\Google\Chrome\User Data\Profile 1" -Recurse -Force
+    echo "Copying Profile"
+    Copy-Item -Path "X:\ChromeData\Profile 1" -Destination "$($env:USERPROFILE)\AppData\Local\Google\Chrome\User Data\" -Recurse -Force
     explorer.exe X:
     Read-Host -Prompt 'press enter when your done'
+    Stop-Process -Name chrome -Force
+    echo "Del old Profile"
+    Remove-Item -Path "X:\ChromeData\Profile 1" -Recurse -Force
+    echo "Copying new Profile"
+    Copy-Item -Path "$($env:USERPROFILE)\AppData\Local\Google\Chrome\User Data\Profile 1" -Destination "X:\ChromeData\" -Recurse -Force
+    echo "Removing Traces & Unmountng X:"
+    Remove-Item -Path "$($env:USERPROFILE)\AppData\Local\Google\Chrome\User Data\" -Recurse -Force
     ./VeraCryptPortable/App/VeraCrypt/VeraCrypt.exe /s /q /d /wipecache /l X
     ./CCleaner/CCleaner.exe /AUTO
     rm -r C:\.catsyndrome\
-    exit
-}else {
-    exit
+
 }
+
+
+Function Check-and-Login{
+
+    if($password_hash -eq $hash_check) {
+        Login-in
+        exit
+    }else {
+        echo "That was wrong you dumb fuck"
+        $password = Read-Host -Prompt 'Input your password' -AsSecureString
+        $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+        $password_hash = Get-StringHash $password "MD5"
+        Check-and-Login
+    }
+}
+
+Check-and-Login
